9813f324883311e01ff76b5fecfe2bc3b830819c
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-07-23 12:02:04 -0500

    Deliverable 1

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -30,23 +30,21 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
-
+let tectonic = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+  "Tectonic Plates": tectonic
 };
 
-// Then we add a control to the map that will allow the user to change which
-// layers are visible.
+// Then we add a control to the map that will allow the user to change which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. We pass the magnitude of the earthquake into two separate functions
-  // to calculate the color and radius.
+  // This function returns the style data for each of the earthquakes we plot on the map. We pass the magnitude of the earthquake into two separate functions to calculate the color and radius.
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -139,9 +137,21 @@ legend.onAdd = function() {
   // Finally, we our legend to the map.
   legend.addTo(map);
 
-
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
-    
+
+  function tectonicStyle() {
+    return {
+      color: "#AD26C1",
+      weight: 3
+    };
+  }
+
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
+    L.geoJson(data, {
+      style: tectonicStyle,
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Name : " + feature.properties.Name + "</h3>");}
+      }).addTo(tectonic);
+    tectonic.addTo(map);
+    });
   });
-});
